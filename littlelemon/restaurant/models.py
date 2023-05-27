f93808722899f11e7b0929345d7f345ed5dba2c9
digesh-gupta <digesh.gupta@gmail.com>
2023-05-28 00:03:57 +0530

    User Authentication

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -4,11 +4,14 @@ from django.db import models
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField(max_length=6)
+    no_of_guests = models.IntegerField()
     bookingdate = models.DateTimeField()
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10,decimal_places=2)
-    inventory = models.IntegerField(max_length=5)
+    inventory = models.IntegerField()
+ 
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
 
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -3,10 +3,17 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 from .models import Menu, Booking
 from .serializers import MenuSerializer, BookingSerializer, UserSerializer
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
 from django.contrib.auth.models import User
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({'message':'This view is protected'})
 
 def index(request):
     return render(request, 'index.html', {})
@@ -14,6 +21,7 @@ def index(request):
 class MenuItemsView(ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
 
 class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
@@ -22,11 +30,12 @@ class SingleMenuItemView(RetrieveUpdateDestroyAPIView):
 class BookingViewSet(ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-
+    permission_classes = [IsAuthenticated]
+    
 class UserViewSet(ModelViewSet):
    queryset = User.objects.all()
    serializer_class = UserSerializer
-   permission_classes = [permissions.IsAuthenticated] 
+   permission_classes = [IsAuthenticated] 
 
 
 
